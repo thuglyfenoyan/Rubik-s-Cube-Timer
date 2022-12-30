3952c57d4306c7e86934964034a41ebfedd4c7c5
Fawaaz Kamali Siddiqui <86437242+thuglyfenoyan@users.noreply.github.com>
2022-12-30 14:10:51 -0500

    Created files and added 2 functions
    - created main.py and scrambles.py
    - added functions for scrambling 2x2 and 3x3 cubes

---
+++ scrambles.py
@@ -0,0 +1,40 @@
+# scrambles.py: Contains functions for generating scrambles for
+# 2x2, 3x3, 4x4, 5x5 and 6x6 Rubik's Cubes
+
+import random
+
+
+def scramble_2x2():                           # 9 moves long
+    shuffle = list()
+    moves = [
+        'R', 'R\'', 'R2',
+        'U', 'U\'', 'U2',
+        'F', 'F\'', 'F2',
+    ]
+
+    shuffle.append(random.choice(moves))
+    while len(shuffle) < 9:
+        random_move = random.choice(moves)
+        if random_move[0] != shuffle[-1][0]:  # moves must not be repeated consecutively
+            shuffle.append(random_move)
+    return shuffle
+
+
+def scramble_3x3():                           # 20 moves long
+    shuffle = list()
+    moves = [
+                'R', 'R\'', 'R2',
+                'L', 'L\'', 'L2',
+                'U', 'U\'', 'U2',
+                'D', 'D\'', 'D2',
+                'F', 'F\'', 'F2',
+                'B', 'B\'', 'B2'
+            ]
+
+    shuffle.append(random.choice(moves))
+    while len(shuffle) < 20:
+        random_move = random.choice(moves)
+        if random_move[0] != shuffle[-1][0]:
+            shuffle.append(random_move)
+    return shuffle
+
