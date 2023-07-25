2bfcc79bdf3da030b8688b6ebde336908c2e229b
Fawaaz Kamali Siddiqui <86437242+thuglyfenoyan@users.noreply.github.com>
2023-07-25 16:06:50 -0400

    Spacebar Timer
    - Timer can start and stop with either the enter button or the spacebar.
    - previous code for timer has not been deleted yet.

--- cube_timer.py
+++ cube_timer.py
@@ -2,14 +2,44 @@
 # taken for the user to solve a puzzle.
 
 import time
+import keyboard
+from msvcrt import getch
 
 
 def time_solve():
-    i = input('Enter the enter button to start timer: ')
-    start = time.time()
-    print('Recording...')
-    i = input('Enter the enter button to stop timer: ')
-    stop = time.time()
-    solve_time = stop - start
+    # i = input('Enter the enter button to start timer: ')
+    # start = time.time()
+    # print('Recording...')
+    # i = input('Enter the enter button to stop timer: ')
+    # stop = time.time()
+
+
+    # print('Enter the space bar to start timer: ')
+    # while True:
+    #     if keyboard.is_pressed(' '):
+    #         start = time.time()
+    #         break
+    #
+    # time.sleep(0.2)     # 2 spacebars can be recorded in a very short amount of time
+    # print('Enter the space bar to stop timer: ')
+    #
+    # while True:
+    #     if keyboard.is_pressed(' '):
+    #         end = time.time()
+    #         break
+
+    to_press = [13, 32]
+    print('Press either the space bar or enter button to start: ')
+    key = ord(getch())
+    if key in to_press:
+        start = time.time()
+
+    time.sleep(0.1)
+    print('Press either the space bar or enter button to stop: ')
+    key = ord(getch())
+    if key in to_press:
+        end = time.time()
+
+    solve_time = end - start
     return round(solve_time, 3)
 
